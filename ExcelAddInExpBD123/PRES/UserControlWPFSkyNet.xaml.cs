c3b94bef8193e0845752e7ee147234c2b8d9ba6c
e1695930 <e1695930@FC412-G3LRX12>
2017-11-15 17:39:25 -0500

    sql exemple mode déconnecté

--- ExcelAddInExpBD123/PRES/UserControlWPFSkyNet.xaml.cs
+++ ExcelAddInExpBD123/PRES/UserControlWPFSkyNet.xaml.cs
@@ -1,6 +1,7 @@
 ﻿
 using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
@@ -9,38 +10,78 @@ using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
+using System.Windows.Forms;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
+// EXEMPLE EN MODE DÉCONNECTÉ
 namespace ExcelAddInExpBD123.PRES {
     /// <summary>
     /// Interaction logic for UserControlWPFSkyNet.xaml
     /// </summary>
-    public partial class UserControlWPFSkyNet : UserControl {
+    public partial class UserControlWPFSkyNet : System.Windows.Controls.UserControl
+    {
 
        // private SkyNetExoEntities db = new SkyNetExoEntities();
 
-        // SqlConnection connexion;
+        SqlConnection connexion;
         // string chaineDeConnexion = "Data Source=ServeurSQL;Initial Catalog = SkyNet;User Id = TEST;Password=AAAaaa111";
 
         public UserControlWPFSkyNet() {
+
             InitializeComponent();
+            string chaineDeConnexion = "Data Source=127.0.0.1;" +
+                                       "Initial Catalog = SkyNet;" + 
+                                       "User Id = PROG1;" +
+                                       "Password=AAAaaa111";
+
+            connexion = new SqlConnection(chaineDeConnexion);
+            // IMPORTANT: pas besoin d'utiliser Open() et Close() pour la connexion
         }
 
-        private void buttonAffEmployesDsList_Click(object sender, RoutedEventArgs e) {
+        //bouton pour afficher contenu de la table employe
+        private void buttonAffColonnesTblEmploye_Click(object sender, RoutedEventArgs e) {
 
             Globals.ThisAddIn.Application.Cursor = Microsoft.Office.Interop.Excel.XlMousePointer.xlWait;
+
+            SqlDataAdapter adapter = new SqlDataAdapter();
+            SqlCommand command = connexion.CreateCommand();
+            command.CommandText = "Select * From dbo.employe";
+            adapter.SelectCommand = command;
+
+            DataSet ds = new DataSet();
+            adapter.Fill(ds, "employe");
+
+            // affiche le nom des colonnes de la table "Employe" ds listBoxNomColonnes:
+            listBoxNomColonnes.Items.Clear();
+            foreach (DataColumn col in ds.Tables["employe"].Columns) {
+                listBoxNomColonnes.Items.Add(new lineItemColonneEmploye { nomColonne = col.ColumnName } );
+            }
+
+            // affiche le contenu des colonnes "id" et "nom" de la table "Employe"
+            DataView vueTrieNom = new DataView(ds.Tables["employe"]);
+            vueTrieNom.Sort = "nom ASC";
+
+            listBoxEmployes.Items.Clear();
+            foreach (DataRowView ligne in vueTrieNom) {
+                //string info = ligne["id"].ToString() + " " + (string)ligne["nom"];
+
+                var lItem = new lineItemEmploye { id = (int)ligne["id"], nom = (string)ligne["nom"] };
+                listBoxEmployes.Items.Add(lItem);
+            }
+
+            Globals.ThisAddIn.Application.Cursor = Microsoft.Office.Interop.Excel.XlMousePointer.xlDefault;
+
+
+
 
             /*
            // var lstEmpl = db.employe.ToList();
             this.listBoxEmployes.ItemsSource = lstEmpl; // les champs affichés définis dans XAML
             */
-
-
-            Globals.ThisAddIn.Application.Cursor = Microsoft.Office.Interop.Excel.XlMousePointer.xlDefault;
 
             // Pour une liste stubbé AVANT création (testing de la présentatiON)
             /* var items = new List<Employe>();
@@ -65,8 +106,67 @@ namespace ExcelAddInExpBD123.PRES {
 
         }
 
+        //bouton pour MAJ nom employe
         private void buttonMAJEmploye_Click(object sender, RoutedEventArgs e) {
             lblMAJ.Content = "";
+
+            // BOUTON pour modifier le nom d'un employé selon son "ID"
+            SqlDataAdapter adapteur = new SqlDataAdapter();
+
+            SqlCommand commande = connexion.CreateCommand();
+            commande.CommandText = "SELECT id,nom FROM dbo.employe";
+            adapteur.SelectCommand = commande;
+
+            DataSet ds = new DataSet();
+            adapteur.FillSchema(ds, SchemaType.Mapped);
+            adapteur.Fill(ds, "employe");
+
+            //-----------------------------------------------------------------------------
+            // Début de la modification du nom d'un employé
+            //-------------------- ---------------------------------------------------------           
+
+            // ÉTAPE 1 - RÉCUPÉRER LA LIGNE À MODIFIER
+            // En recherchant le ID, la variable "lignes" va contenir une seule ligne de données
+            DataRow[] lignes = ds.Tables["employe"].Select("id = " + int.Parse(txtIDEmploye.Text));
+            // La variable "ligneAModifier" contient une seule ligne
+            DataRow ligneAModifier = lignes[0];
+
+            // Valeur du nom de l'employé avant la modification officielle
+            string ancienNom = (string)ligneAModifier["nom"];
+
+            // ÉTAPE 2 - ÉDITER LA LIGNE À MODIFIER
+            // Bloc de code "BeginEdit - EndEdit"
+            ligneAModifier.BeginEdit();
+            ligneAModifier["nom"] = txtNomEmploye.Text;
+            ligneAModifier.EndEdit();
+
+            // Valeur du nom du département après la modification officielle
+            string nouveauNom = (string)ligneAModifier["nom"];
+
+            // Si ID = 7934 alors le DataSet est modifié !!!
+            Console.WriteLine(ds.Tables["employe"].Rows[13]["nom"]);
+
+            // ÉTAPE 3 - VALIDER LA MODIFICATION
+            SqlCommandBuilder builder1 = new SqlCommandBuilder(adapteur);
+            adapteur.UpdateCommand = builder1.GetUpdateCommand();
+            adapteur.Update(ds, "employe");
+
+            // ÉTAPE 4 - MESSAGE POUR VÉRIFIER DE LA MODIFICATION
+            System.Windows.Forms.MessageBox.Show("Le nom du département a été modifié.",
+                            "ID = " + (lignes[0]["id"]).ToString() +
+                            ", " +
+                            "Ancien NOM = " + ancienNom +
+                            ", " +
+                            "Nouveau NOM = " + nouveauNom,
+                            MessageBoxButtons.OK,
+                            MessageBoxIcon.Information);
+
+            //-----------------------------------------------------------------------------
+            // Fin de la modification du nom d'un l'employé
+            //-----------------------------------------------------------------------------
+
+
+
 
             /*
             // VALIDATION
@@ -124,6 +224,16 @@ namespace ExcelAddInExpBD123.PRES {
         }
 
         private void listBoxEmployes_SelectionChanged(object sender, SelectionChangedEventArgs e) {
+
+            var buff = sender as System.Windows.Controls.ListBox;
+            var sel = (lineItemEmploye)buff.SelectedItem;
+
+            if (sel != null)
+            {
+                txtIDEmploye.Text = sel.id.ToString();
+                txtIDEmployeAsuppr.Text = sel.id.ToString();
+                txtNomEmploye.Text = sel.nom.ToString();
+            }
             /*
             var buff = sender as ListBox;
             var sel = (employe)buff.SelectedItem;
@@ -135,17 +245,100 @@ namespace ExcelAddInExpBD123.PRES {
             */
         }
 
-        private void buttonINSEmp_Click(object sender, RoutedEventArgs e) {
-
-        }
-
         private void buttonDELEmp_Click(object sender, RoutedEventArgs e) {
+            // BOUTON pour supprimer un employé selon son "ID"
+            SqlDataAdapter adapteur = new SqlDataAdapter();
+
+            SqlCommand commande = connexion.CreateCommand();
+            commande.CommandText = "SELECT id FROM dbo.employe";
+            adapteur.SelectCommand = commande;
+
+            DataSet ds = new DataSet();
+            adapteur.FillSchema(ds, SchemaType.Mapped);
+            adapteur.Fill(ds, "employe");
+
+            //-----------------------------------------------------------------------------
+            // Début de la suppression d'un employé
+            //-------------------- ---------------------------------------------------------           
+            // ÉTAPE 1 - RÉCUPÉRER LA LIGNE À SUPPRIMER
+            // En recherchant le ID, la variable "lignes" va contenir une seule ligne de données
+            DataRow[] lignes = ds.Tables["employe"].Select("id = " + int.Parse(txtIDEmployeAsuppr.Text));
+
+
+            // La variable "ligneASupprimer" contient une seule ligne
+            DataRow ligneASupprimer = lignes[0];
+
+
+            // ÉTAPE 2 - LA LIGNE À SUPPRIMER
+            // Bloc de code "BeginEdit - EndEdit"
+            ligneASupprimer.BeginEdit();
+            ligneASupprimer.Delete();
+            ligneASupprimer.EndEdit();
+
+
+            // ÉTAPE 3 - VALIDER LA MODIFICATION
+            SqlCommandBuilder builder1 = new SqlCommandBuilder(adapteur);
+            adapteur.UpdateCommand = builder1.GetUpdateCommand();
+            adapteur.Update(ds, "employe");
+
+
+            //-----------------------------------------------------------------------------
+            // Fin de la suppression d'un employé
+            //-----------------------------------------------------------------------------
+        }
+
+        private void buttonINSEmp_Click(object sender, RoutedEventArgs e)
+        {
+            SqlDataAdapter adapteur = new SqlDataAdapter();
+
+            SqlCommand commande = connexion.CreateCommand();
+            commande.CommandText = "SELECT id, id_departement, nom FROM dbo.employe";
+            adapteur.SelectCommand = commande;
+
+            DataSet ds = new DataSet();
+            adapteur.FillSchema(ds, SchemaType.Mapped);
+            adapteur.Fill(ds, "employe");
+            // BOUTON pour insérer un employé avec son "ID", "ID_DEPARTEMENT" et "NOM"
+
+            //-----------------------------------------------------------------------------
+            // Début de l'insertion d'un employé
+            //-------------------- ---------------------------------------------------------           
+            // ÉTAPE 1 - DÉCLARATION DE LA NOUVELLE LIGNE
+            // La variable "nouvelleLigne" va contenir les informations de l'employé
+            DataRow nouvelleLigne = ds.Tables["employe"].NewRow();
+
+
+            // ÉTAPE 2 - LA LIGNE À INSÉRER
+            // Bloc de code "BeginEdit - EndEdit"
+            nouvelleLigne.BeginEdit();
+                nouvelleLigne["id"] = txtIDEmployeAinserer.Text;
+                nouvelleLigne["id_departement"] = txtIDDepartement.Text;
+                nouvelleLigne["nom"] = txtNomEmpAinserer.Text;
+                ds.Tables["employe"].Rows.Add(nouvelleLigne);
+            nouvelleLigne.EndEdit();
+
+            // ÉTAPE 3 - VALIDER LA MODIFICATION
+            SqlCommandBuilder builder3 = new SqlCommandBuilder(adapteur);
+            adapteur.InsertCommand = builder3.GetInsertCommand();
+            adapteur.Update(ds, "employe");
+
+
+            //-----------------------------------------------------------------------------
+            // Fin de l'insertion d'un employé
+            //-----------------------------------------------------------------------------
 
         }
 
-        private void buttonAffColonnesTblEmploye_Click(object sender, RoutedEventArgs e) {
 
-        }
+    }
+    class lineItemEmploye
+    {
+        public int id { get; set; }
+        public string nom { get; set; }
+    }
+    class lineItemColonneEmploye
+    {
+        public string nomColonne { get; set; }
     }
 }
 
